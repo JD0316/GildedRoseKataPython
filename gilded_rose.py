4bb8a8470465fce875b660846b5614d117caa9a7
JD0316 <dingjin6324@gmail.com>
2025-02-12 16:58:07 -0800

    Update gilded_rose.py

--- gilded_rose.py
+++ gilded_rose.py
@@ -1,52 +1,4 @@
 # -*- coding: utf-8 -*-
-from abc import ABC, abstractmethod
-
-class ItemStrategy(ABC):
-    def __init__(self, item):
-        self.item = item
-
-    @abstractmethod
-    def update_quality(self):
-        pass
-        
-class NormalItemStrategy(ItemStrategy):
-    def update_quality(self):
-        if self.item.quality > 0:
-            self.item.quality -= 1
-        self.item.sell_in -= 1
-        if self.item.sell_in < 0 and self.item.quality > 0:
-            self.item.quality -= 1
-
-class AgedBrieStrategy(ItemStrategy):
-    def update_quality(self):
-        if self.item.quality < 50:
-            self.item.quality += 1
-        self.item.sell_in -= 1
-
-class SulfurasStrategy(ItemStrategy):
-    def update_quality(self):
-        pass  
-
-class BackstagePassStrategy(ItemStrategy):
-    def update_quality(self):
-        if self.item.sell_in > 10:
-            self.item.quality += 1
-        elif self.item.sell_in > 5:
-            self.item.quality += 2
-        elif self.item.sell_in > 0:
-            self.item.quality += 3
-        else:
-            self.item.quality = 0
-        self.item.sell_in -= 1
-
-class ConjuredItemStrategy(ItemStrategy):
-    def update_quality(self):
-        if self.item.quality > 0:
-            self.item.quality -= 2 
-        self.item.sell_in -= 1
-        if self.item.sell_in < 0 and self.item.quality > 0:
-            self.item.quality -= 2  
-
 
 class Item:
     """ DO NOT CHANGE THIS CLASS!!!"""
@@ -58,33 +10,8 @@ class Item:
     def __repr__(self):
         return "%s, %s, %s" % (self.name, self.sell_in, self.quality)
 
-class GildedRose:
-    def __init__(self, items):
-        self.items = items
 
-    def update_quality(self):
-        for item in self.items:
-            strategy = self.get_strategy(item)
-            strategy.update_quality()
-
-    def get_strategy(self, item):
-        if item.name == "Aged Brie":
-            return AgedBrieStrategy(item)
-        elif item.name == "Sulfuras, Hand of Ragnaros":
-            return SulfurasStrategy(item)
-        elif "Backstage passes" in item.name:
-            return BackstagePassStrategy(item)
-        elif "Conjured" in item.name:  # 确保 Conjured 物品使用正确策略
-            return ConjuredItemStrategy(item)
-        else:
-            return NormalItemStrategy(item)
-
-
-    
-
-
-
-"""class GildedRose(object):
+class GildedRose(object):
 
     def __init__(self, items: list[Item]):
         # DO NOT CHANGE THIS ATTRIBUTE!!!
@@ -92,30 +19,35 @@ class GildedRose:
 
     def update_quality(self):
         for item in self.items:
+            if item.name.startswith("Conjured"):
+                degrade = 2  # Conjured 物品质量下降速度翻倍
+            else:
+                degrade = 1  # 普通物品的默认质量下降
+
             if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert":
                 if item.quality > 0:
                     if item.name != "Sulfuras, Hand of Ragnaros":
-                        item.quality = item.quality - 1
+                        item.quality -= degrade
             else:
                 if item.quality < 50:
-                    item.quality = item.quality + 1
+                    item.quality += 1
                     if item.name == "Backstage passes to a TAFKAL80ETC concert":
-                        if item.sell_in < 11:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
-                        if item.sell_in < 6:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
+                        if item.sell_in < 11 and item.quality < 50:
+                            item.quality += 1
+                        if item.sell_in < 6 and item.quality < 50:
+                            item.quality += 1
+
             if item.name != "Sulfuras, Hand of Ragnaros":
-                item.sell_in = item.sell_in - 1
+                item.sell_in -= 1
+
             if item.sell_in < 0:
                 if item.name != "Aged Brie":
                     if item.name != "Backstage passes to a TAFKAL80ETC concert":
                         if item.quality > 0:
                             if item.name != "Sulfuras, Hand of Ragnaros":
-                                item.quality = item.quality - 1
+                                item.quality -= degrade
                     else:
-                        item.quality = item.quality - item.quality
+                        item.quality = 0
                 else:
                     if item.quality < 50:
-                        item.quality = item.quality + 1"""
+                        item.quality += 1
