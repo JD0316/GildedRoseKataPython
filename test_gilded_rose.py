9815cde20f7c3d4813294283b338da4f57e1b5c4
JD0316 <dingjin6324@gmail.com>
2025-02-12 17:26:22 -0800

    Update test_gilded_rose.py

--- test_gilded_rose.py
+++ test_gilded_rose.py
@@ -57,11 +57,11 @@ class GildedRoseTest(unittest.TestCase):
         gilded_rose.update_quality()
         self.assertEqual(items[0].quality, 0)  
 
-    """def test_conjured_quality_decreases_twice_as_fast(self):
+    def test_conjured_quality_decreases_twice_as_fast(self):
         items = [Item("Conjured Mana Cake", 5, 10)]
         gilded_rose = GildedRose(items)
         gilded_rose.update_quality()
-        self.assertEqual(items[0].quality, 8)"""
+        self.assertEqual(items[0].quality, 8)
 
     def test_conjured_quality_decreases_twice_as_fast_after_sellin(self):
         items = [Item("Conjured Mana Cake", 0, 10)]
